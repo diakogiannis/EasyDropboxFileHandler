c15f9a12f7221cfa9825b079a50275a5f975eb73
Alexius DIAKOGIANNIS <alexius@jee.gr>
2017-11-15 13:24:36 +0200

    Initial Commit

---
+++ src/main/java/io/znk/dropbox/files/FileHandlerInterface.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2017 Alexius Diakogiannis.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.znk.dropbox.files;
+
+import com.dropbox.core.DbxException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Locale;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author Alexius Diakogiannis [alexius at jee.gr]
+ */
+public interface FileHandlerInterface {
+
+    /**
+     * Creates a directory using a default Locale the Itallian
+     * @param path The path in dropbox there the directory will be created
+     * @param name Directory name
+     * @return The directory path
+     * @throws DbxException
+     */
+    String createDirectory(String path, String name) throws DbxException;
+
+    /**
+     *
+     * @param path The path in dropbox there the directory will be created
+     * @param name Directory name
+     * @param locale The locale
+     * @return The directory path
+     * @throws DbxException
+     */
+    String createDirectory(String path, String name, Locale locale) throws DbxException;
+
+    /**
+     *
+     * @param path The path in dropbox there the file will be deleted
+     * @throws DbxException
+     */
+    void deleteFile(String path) throws DbxException;
+
+    /**
+     *
+     * @param folder folder location
+     * @param recursively recursive list flag
+     * @return List with file paths
+     * @throws DbxException
+     */
+    List<String> listFiles(String folder, boolean recursively) throws DbxException;
+
+    /**
+     *
+     * @param path The path in dropbox there the file exists
+     * @return java.io.InputStream with file contents
+     * @throws DbxException
+     */
+    InputStream readFile(String path) throws DbxException;
+
+    /**
+     *
+     * @param path The local path with the file to be uploaded
+     * @param remotePath The path in dropbox there the file will be uploaded
+     * @param name filename for remote use
+     * @throws FileNotFoundException
+     * @throws DbxException
+     * @throws IOException
+     */
+    void uploadFile(String path, String remotePath, String name) throws FileNotFoundException, DbxException, IOException;
+
+    /**
+     *
+     * Method created for use with Spring MVC Framework that uses org.springframework.web.multipart.MiltipartFile
+     * @see org.springframework.web.multipart
+     * @param multipartFile The file contents
+     * @param remotePath The path in dropbox there the file will be uploaded
+     * @throws IOException
+     * @throws DbxException
+     */
+    void uploadFile(MultipartFile multipartFile, String remotePath) throws IOException, DbxException;
+    
+}
---
+++ src/test/java/io/znk/dropbox/files/connection/DxIntegrationTest.java
@@ -0,0 +1,119 @@
+/*
+ *
+ * Copyright {2017} {Alexius Diakogiannis}
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+package io.znk.dropbox.files.connection;
+
+import com.dropbox.core.DbxException;
+import io.znk.dropbox.files.FileHandler;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import org.junit.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+/**
+ *
+ * @author DiakogiannisA
+ */
+public class DxIntegrationTest {
+
+    Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    String appId;
+    String accessTokens;
+
+    public DxIntegrationTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        try {
+            InputStream stream = classLoader.getResourceAsStream("test.properties");
+            Properties properties = new Properties();
+            properties.load(stream);
+            appId = properties.getProperty("dropbox.appId");
+            accessTokens = properties.getProperty("dropbox.accessTokens");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void connect() throws DbxException, IOException {
+        if (StringUtils.isEmpty(appId) || StringUtils.isEmpty(accessTokens)) {
+            LOG.error("test.properties has no credentials for both appId and accessToken, skiping test...");
+            Assert.assertTrue(true);
+        } else {
+            DropboxDbxClient dropboxDbxClient = new DropboxDbxClient(appId, accessTokens);
+
+            FileHandler fileHandler = new FileHandler(dropboxDbxClient.getClient());
+
+            String filename = "test-" + UUID.randomUUID().toString();
+            File temp = File.createTempFile(filename, ".txt");
+
+            // Upload "test.txt" to Dropbox
+            fileHandler.uploadFile(temp.getAbsolutePath(), "/", filename + ".txt");
+
+            // Get files and folder metadata from Dropbox root directory
+            List<String> fileList = fileHandler.listFiles("", false);
+
+            Assert.assertEquals(
+                    fileList.stream()
+                            .filter(m -> ("/" + filename + ".txt").equals(m))
+                            .count(),
+                    1);
+            //cleanup
+            fileHandler.deleteFile("/" + filename + ".txt");
+
+            //verify
+            fileList = fileHandler.listFiles("", false);
+            Assert.assertEquals(
+                    fileList.stream()
+                            .filter(m -> ("/" + filename + ".txt").equals(m))
+                            .count(),
+                    0);
+        }
+    }
+}
